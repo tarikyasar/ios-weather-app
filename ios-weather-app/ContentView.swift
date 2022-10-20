1eaf341b879e0df3a6b9ea4e52fcefa84001d0c0
Tarik Yasar <m.tarikyasar@gmail.com>
2022-10-20 19:02:18 +0300

    Initial Commit

---
+++ ios-weather-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios-weather-app
+//
+//  Created by Tarik Yasar on 20.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-weather-app/ios_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_weather_appApp.swift
+//  ios-weather-app
+//
+//  Created by Tarik Yasar on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
